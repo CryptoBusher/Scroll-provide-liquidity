cf971f8e68c8e01e353c8c239ad88bc79bd68187
CryptoBusher <garlicplan@gmail.com>
2024-06-30 13:52:55 +0300

    Fixed minor bug

--- _config.js
+++ _config.js
@@ -26,12 +26,14 @@ export const config = {
             minDeposit: 0.013,                                  // Минимальный депозит (>0)
             remainingBalance: [0.001, 0.002],                   // Сколько должно оставаться на кошельке после депозита (min, max). Депозит будет сделан, если баланс >= (minDeposit + remainingBalance max) 
             roundToDecimals: [2, 4],                            // Округление суммы депозита (min, max). Например, при значении 2: 0.123456789 -> 0.123, 0.0123456789 -> 0.0123
+            safetyTreshold: 0.5                                 // Запас при проверке доступности пула (в пуле должно будет хватать места на amountToDeposi + safetyTreshold). Чем выше значение - тем меньше вероятность не успеть (0+)
         },
         USDC: {
             depositEnabled: false,                              // Депозитить USDC (true, false)
             minDeposit: 0.1,                                    // Минимальный депозит (>0)
             remainingBalance: [0, 0],                           // Сколько должно оставаться на кошельке после депозита (min, max)
             roundToDecimals: [1, 2],                            // Округление суммы депозита (min, max)
+            safetyTreshold: 1000                                // Запас при проверке доступности пула (0+)
         }
     },
 
